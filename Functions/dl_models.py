b14168afca78c438ee8f06f08a04f014d0751e58
José Jaén <87200089+jose-jaen@users.noreply.github.com>
2022-11-25 08:30:07 +0100

    Update dl_models.py

--- Functions/dl_models.py
+++ Functions/dl_models.py
@@ -143,9 +143,9 @@ def BNN(train, valid, test):
         preds = torch.stack(preds)
         means = preds.mean(axis=0)
         stds = preds.std(axis=0)
-        ci_upper = means + (std_multiplier * stds)
-        ci_lower = means - (std_multiplier * stds)
-        ic_acc = (ci_lower <= y) * (ci_upper >= y)
+        ci_upper = means + (std_multiplier*stds)
+        ci_lower = means - (std_multiplier*stds)
+        ic_acc = (ci_lower <= y)*(ci_upper >= y)
         ic_acc = ic_acc.float().mean()
         return ic_acc, (ci_upper >= y).float().mean(), (ci_lower <= y).float().mean()
 
