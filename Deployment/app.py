06123a3b43583fa3b99e5466a2eaf3b8cc6e9c83
José Jaén <87200089+jose-jaen@users.noreply.github.com>
2023-01-03 21:02:57 +0100

    Update app.py

--- Deployment/app.py
+++ Deployment/app.py
@@ -10,7 +10,7 @@ import requests
 
 # Set up header and brief description
 with st.container():
-    st.title('Airbnb Prices Predictor')
+    st.title('Airbnb Price Predictor')
     st.markdown('Simulate real estate market asset valuations with AI!')
     st.markdown('Provide data about your Airbnb listing and get predictions!')
 
