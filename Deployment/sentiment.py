e43b3a7fd7f20868733828af96671cf23c87d792
José Jaén <87200089+jose-jaen@users.noreply.github.com>
2023-01-16 18:02:21 +0100

    Update sentiment.py

--- Deployment/sentiment.py
+++ Deployment/sentiment.py
@@ -1,7 +1,10 @@
 from transformers import AutoModelForSequenceClassification
 from transformers import TFAutoModelForSequenceClassification
 from transformers import AutoTokenizer, AutoConfig
+from transformers import logging
 from scipy.special import softmax
+
+logging.set_verbosity_error()
 
 # Preprocess text (username and link placeholders)
 def preprocess(text):
